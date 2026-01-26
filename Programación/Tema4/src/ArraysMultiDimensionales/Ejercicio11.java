350925ba7df2926bcc9db28d20e11497cfd514aa
Enrique <enrique.fernandez34@educa.madrid.org>
2026-01-26 09:19:29 +0100

    1/26/2026 java

--- Programación/Tema4/src/ArraysMultiDimensionales/Ejercicio11.java
+++ Programación/Tema4/src/ArraysMultiDimensionales/Ejercicio11.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 public class Ejercicio11 {
     public static void main(String[] args) {
-        int[][][] matriz = new int[9][9][9];
+        int[][][] matriz = new int[6][6][6];
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
                 for (int j2 = 0; j2 < matriz[j].length; j2++) {
 
                     if (
-                        i == j || i == j2 || j == j2 || i == 0 || j == 0 || j2 == 0 || i == matriz.length || j == matriz[i].length || j2 == matriz[j].length
+                        i == 0 || j == 0 || j2 == 0 || i == matriz.length - 1 || j == matriz[i].length - 1 || j2 == matriz[j].length - 1
                     ) {
                         
                         matriz[i][j][j2] = 1;
