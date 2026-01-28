9673ebbbb7c04aaab8833dc70ed70f0a224123c8
Enrique <enrique.fernandez34@educa.madrid.org>
2026-01-28 13:16:13 +0100

    1/28/2026 java2

--- Programación/Tema4/src/ArraysMultiDimensionales/Ejercicio12.java
+++ Programación/Tema4/src/ArraysMultiDimensionales/Ejercicio12.java
@@ -5,13 +5,22 @@ import java.util.Arrays;
 public class Ejercicio12 {
 
     public static void main(String[] args) {
-        
-        String[] fila1 = {"?","Irene","Nayara","Jesús","Alejandro","Jaime","Aitor","Adrian"};
-        String[] fila2 = {"Dario","Ivan","Raul","Carlos","Samuel","Zakaria", "?","Marta"};
-        String[] fila3 = {"Victor","Alvaro","Alonso","Robert","David","Nieves"};
-        String[] fila4 = {"Izan","Adrian","Carlos","Lucia","Rodrigo"};
+
+        String[] fila1 = {"Irene","Nayara","Jesús","Alejandro","Jaime","Aitor","Adrian"};
+        String[] fila2 = {"Dario","Ivan","Raul","Carlos","Samuel","Zakaria","Lucia"};
+
+        /* Segunda manera */
+
+        String[] fila3,fila4;
+
+        fila3 = new String[]{"Victor","Alvaro","Alonso","Enrique","Robert","David","Nieves"};
+        fila4 = new String[]{"Izan","Alex","Carlos","Marta","Rodrigo","Lucia"};
 
         String[][] clase = {fila1, fila2, fila3, fila4};
+
+        /* Tercera */
+
+        clase[4] = new String[] {"pepito","agustin"};
 
         System.out.println(Arrays.deepToString(clase));
 
