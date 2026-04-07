cdf3668e90bcc57f40c2270ffbd860b7b0224468
Enrique <enrique.fernandez34@educa.madrid.org>
2026-04-07 11:04:19 +0200

    04/07/2026 java cesar

---
+++ Programación/Tema9/Ficheros/CesarEncript.java
@@ -0,0 +1,31 @@
+package Tema9.Ficheros;
+
+import java.util.Scanner;
+
+public class CesarEncript {
+
+    static final String ABECEDARIO = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+    // Juega con el valor ASCII
+    // Con la clase fileReader miraras cada letra, en vez de un palabra
+
+    static Scanner scIn = new Scanner(System.in);
+    
+    int defineROT(int rot) {
+        if (rot > ABECEDARIO.length()/2 || rot < 1) {return 0;}
+        return rot;
+    }
+
+    String encode(String in) {
+        int num = Integer.parseInt(in);
+        if ()
+            // JUEGA CON EL VALOR ASCII
+    }
+    
+    public static void main(String[] args) {
+        while (scIn.hasNext()) {
+            System.out.println("¿Que archivo quieres encodificar?(El archivo tiene que estar dentro de ficheros)");
+
+
+        }
+    }
+}
---
+++ Programación/Tema9/Ficheros/pruebas.java
@@ -0,0 +1,8 @@
+package Tema9.Ficheros;
+
+public class pruebas {
+    public static void main(String[] args) {
+        byte a = '!';
+        System.out.println(a);
+    }
+}
